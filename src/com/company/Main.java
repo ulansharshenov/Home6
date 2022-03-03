c4f5e718ab3529abc215d582501cea4df1398833
ulansharshenov <97440389+ulansharshenov@users.noreply.github.com>
2022-03-03 18:40:31 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 5;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+        System.out.println(num + "!" + " = " + factorial);
+    }
+}
